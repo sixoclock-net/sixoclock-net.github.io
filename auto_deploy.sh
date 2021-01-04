63ff4f98679dedd78b5c5c5fd63f5deccd3f2b02
Yu-L <huangyeluwei@163.com>
2021-01-04 14:22:55 +0800

    添加自动获取名词解释目录结构功能

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,1 +1,16 @@
-mkdocs gh-deploy
+
+DOC_DIR='docs'
+
+cat mkdocs.yml.base
+for category in 名词解释
+do
+    echo "  - ${category}:" # 缩进用2个空格
+    for subdir in `ls ${DOC_DIR}/${category}/`
+    do
+        echo "    - ${subdir}:"
+        for file in `ls ${DOC_DIR}/${category}/${subdir}`; do echo "      - ${category}/${subdir}/${file}";done
+    done
+    
+done
+cat mkdocs.yml.tail
+# mkdocs gh-deployw
